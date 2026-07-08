49a2ae399f80965792dde9385e255aea5d729c4f
kp5.choi@samsung.com <kp5.choi@samsung.com>
2026-07-08 15:38:52 +0900

    Bound tile size to picture dimensions

--- src/oapv_param.c
+++ src/oapv_param.c
@@ -507,6 +507,7 @@ static int enc_update_param_tile(oapve_ctx_t* ctx, oapve_param_t* param)
     int tile_w, tile_h;
 
     oapv_assert_rv(param->tile_w >= OAPV_MIN_TILE_W && param->tile_h >= OAPV_MIN_TILE_H, OAPV_ERR_INVALID_ARGUMENT);
+    oapv_assert_rv(param->tile_w <= ctx->w && param->tile_h <= ctx->h, OAPV_ERR_INVALID_ARGUMENT);
     oapv_assert_rv((param->tile_w & (OAPV_MB_W - 1)) == 0 && (param->tile_h & (OAPV_MB_H - 1)) == 0, OAPV_ERR_INVALID_ARGUMENT);
 
     if (oapv_div_round_up(ctx->w, param->tile_w) > OAPV_MAX_TILE_COLS) {
